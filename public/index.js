cfa8d8eb9e68d2cba9998552aec04d0f5ab38602
Yoon Seo Lee <yoonseo.lee@rsupport.com>
2018-10-29 18:18:46 +0900

    발레리나 캐릭터 추가에 따른 수정

--- public/index.js
+++ public/index.js
@@ -1,43 +1,12 @@
 'use strict';
 
-const setBackground = () => {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const body = document.getElementsByTagName('body');
-    // const scene = document.getElementsByClassName('js-carousel__scene')[0];
-    // console.log(scene);
-
-    setBgSize(body);
-    setBgColor(body);
-    
-    function setBgSize(target){
-      target[0].style.width = windowWidth + 'px';
-      target[0].style.height = windowHeight + 'px';
-    }   
-
-    function setBgColor(target){
-      const randomColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-      target[0].style.backgroundColor = randomColor;
-    }
-    
-    // function setScenePosition(){}
-};
-
-const setCarousel = () => {
-  const carousel = document.getElementById('js-carousel__scene');
-  const casouselCell = document.querySelectorAll('.carousel__cell');
-
-};
-
-
-setBackground();
-var angle = 60 * 6 * -1;
-
-// var test =   Math.round( ( 420 / 2) / Math.tan( Math.PI / 6 ) );
-console.log(angle);
-
-
-
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const body = document.getElementsByTagName('body');
+  const setBodySize = (() => {
+    body[0].style.width = windowWidth + 'px';
+    body[0].style.height = windowHeight + 'px';
+  })();
 
 /*
 회전목마
